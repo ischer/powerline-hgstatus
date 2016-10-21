f3b32a885bb0e4b148cfaafda2d8773e4166b9bf
Ian Scherer <ischer@users.noreply.github.com>
2016-10-21 16:26:56 -0400

    baseline functionality complete

--- powerline_hgstatus/segments.py
+++ powerline_hgstatus/segments.py
@@ -3,47 +3,59 @@
 from powerline.segments import Segment, with_docstring
 from powerline.theme import requires_segment_info
 from subprocess import PIPE, Popen
-import os
+import string
 
 
 @requires_segment_info
 class HgStatusSegment(Segment):
 
-    def get_hg_status(self):
+    def execute(self, pl, command):
+        pl.debug('Executing command: %s' % ' '.join(command))
+
+        proc = Popen(command, stdout=PIPE, stderr=PIPE)
+        out, err = [item.decode('utf-8') for item in proc.communicate()]
+
+        if out:
+            pl.debug('Command output: %s' % out.strip(string.whitespace))
+        if err:
+            pl.debug('Command errors: %s' % err.strip(string.whitespace))
+
+        return (out.splitlines(), err.splitlines())
+
+    def get_hg_status(self, pl, cwd):
         has_modified_files = False
         has_untracked_files = False
         has_missing_files = False
 
-        p = subprocess.Popen(['hg', 'status'], stdout=subprocess.PIPE)
-        output = p.communicate()[0].decode("utf-8")
+        output, err = self.execute(pl, ['hg', 'status', '-R', cwd])
 
-        for line in output.split('\n'):
+        if len(output) == 0:
+            return has_modified_files, has_untracked_files, has_missing_files
+
+        for line in output:
             if line == '':
                 continue
             elif line[0] == '?':
                 has_untracked_files = True
             elif line[0] == '!':
                 has_missing_files = True
-            else:
+            elif line[0] == 'M':
                 has_modified_files = True
+
         return has_modified_files, has_untracked_files, has_missing_files
 
-    def get_hg_branch(self):
-        branch = os.popen('hg branch 2> /dev/null').read().rstrip()
-        if len(branch) == 0:
+    def get_hg_branch(self, pl, cwd):
+        output, err = self.execute(pl, ['hg', 'branch', '-R', cwd])
+
+        if len(output) == 0:
             return False
-        has_modified_files, has_untracked_files, has_missing_files = self.get_hg_status()
-        if has_modified_files or has_untracked_files or has_missing_files:
-            extra = ''
-            if has_untracked_files:
-                extra += '+'
-            if has_missing_files:
-                extra += '!'
-            branch += (' ' + extra if extra != '' else '')
+
+        branch = output[0]
+
         return branch
 
-    def build_segments(self, branch):
-        has_modified_files, has_untracked_files, has_missing_files = self.get_hg_status()
+    def build_segments(self, branch, pl, cwd):
+        has_modified_files, has_untracked_files, has_missing_files = self.get_hg_status(pl, cwd)
 
         if has_missing_files or has_modified_files or has_untracked_files:
             branch_group = 'hgstatus_branch_dirty'
@@ -55,29 +67,28 @@ class HgStatusSegment(Segment):
         ]
 
         if has_missing_files:
-            segments.append({'contents': ' ✖ %d', 'highlight_groups': ['hgstatus_missing', 'hgstatus'], 'divider_highlight_group': 'hgstatus:divider'})
+            segments.append({'contents': ' ✖', 'highlight_groups': ['hgstatus_missing', 'hgstatus'], 'divider_highlight_group': 'hgstatus:divider'})
         if has_modified_files:
-            segments.append({'contents': ' ✚ %d', 'highlight_groups': ['hgstatus_modified', 'hgstatus'], 'divider_highlight_group': 'hgstatus:divider'})
+            segments.append({'contents': ' ✚', 'highlight_groups': ['hgstatus_modified', 'hgstatus'], 'divider_highlight_group': 'hgstatus:divider'})
         if has_untracked_files:
-            segments.append({'contents': ' … %d', 'highlight_groups': ['hgstatus_untracked', 'hgstatus'], 'divider_highlight_group': 'hgstatus:divider'})
+            segments.append({'contents': ' …', 'highlight_groups': ['hgstatus_untracked', 'hgstatus'], 'divider_highlight_group': 'hgstatus:divider'})
 
         return segments
 
     def __call__(self, pl, segment_info):
-        pl.debug('Running hgstatus %s')
+        pl.debug('Running hgstatus...')
 
         cwd = segment_info['getcwd']()
 
         if not cwd:
             return
 
-        branch = self.get_hg_branch()
+        branch = self.get_hg_branch(pl, cwd)
 
         if not branch:
             return
 
-        return self.build_segments(branch)
+        return self.build_segments(branch, pl, cwd)
 
 
-hgstatus = with_docstring(HgStatusSegment(),
-'''Return the status of a Mercurial working copy.''')
+hgstatus = with_docstring(HgStatusSegment(), '''Return the status of a Mercurial working copy.''')
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name         = 'powerline-hgstatus',
     description  = 'A Powerline segment for showing the status of a Mercurial working copy',
-    version      = '0.0.2',
+    version      = '0.1.0',
     keywords     = 'powerline hg status prompt',
     license      = 'MIT',
     author       = 'Ian Scherer',
@@ -15,7 +15,7 @@ setup(
     classifiers  = [
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: MIT License',
         'Topic :: Terminals'
     ]
 )
