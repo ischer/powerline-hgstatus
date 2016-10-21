90d14af03e1cce98e7ea133c4dfd50912e4d78fa
Ian Scherer <ischer@users.noreply.github.com>
2016-10-21 17:20:37 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name         = 'powerline-hgstatus',
     description  = 'A Powerline segment for showing the status of a Mercurial working copy',
-    version      = '0.1.1',
+    version      = '0.1.2',
     keywords     = 'powerline hg status prompt',
     license      = 'MIT',
     author       = 'Ian Scherer',
@@ -15,7 +15,7 @@ setup(
     classifiers  = [
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: MIT License',
+        'License :: OSI Approved :: MIT License',
         'Topic :: Terminals'
     ]
 )
